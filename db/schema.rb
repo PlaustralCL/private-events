f74393a35a3f2297fc0a2e7c1e43ef69c9a7e48f
PlaustralCL <plaustralcl@gmail.com>
2022-03-25 06:42:14 -0400

    Set up Devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_111825) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_25_104021) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
