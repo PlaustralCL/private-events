6a72909899f96609b1dda810ad85176eba1a7e82
PlaustralCL <plaustralcl@gmail.com>
2022-03-26 16:54:14 -0400

    Allow users to create new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,24 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date)
+  end
 end
