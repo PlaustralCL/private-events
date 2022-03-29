8e6c65743587986aa90db2e671df1d6d3e1537a9
PlaustralCL <plaustralcl@gmail.com>
2022-03-29 07:24:49 -0400

    Show name instead of email
    Added the name field to the User table and updated the application
    controller to accpept the name field. Updated the Views to show
    the name instead of email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_145754) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_105627) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "location"
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_145754) do
     t.datetime "updated_at", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
