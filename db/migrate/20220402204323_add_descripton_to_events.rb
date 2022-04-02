1dc0f0c3e3ff9e26de53b1ff296b3aaea3e50775
PlaustralCL <plaustralcl@gmail.com>
2022-04-02 16:48:44 -0400

    Add event descriptions to seed data

---
+++ db/migrate/20220402204323_add_descripton_to_events.rb
@@ -0,0 +1,5 @@
+class AddDescriptonToEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_column :events, :description, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,15 @@ names = %w[Emma Olivia Ava Isabella Sophia Liam Noah James Logan]
 years = (2021..2031).to_a.map(&:to_s)
 months = (1..12).to_a.map(&:to_s).map { |month| month.rjust(2, "0") }
 days = (1..28).to_a.map(&:to_s).map { |day| day.rjust(2, "0") }
-event_descriptions = %w[Equinox Soltice Spring Summer Winter Autumn Sun Snow Rain River Farm Flower Fruit Music Cow Duck Heritage Water Stone Fire]
+event_names = %w[Equinox Soltice Spring Summer Winter Autumn Sun Snow Rain River Farm Flower Fruit Music Cow Duck Heritage Water Stone Fire]
 event_types = %w[Festival Anniversary Competion Fair Feast Gala Holiday Carnival Fiesta Jubilee Celebration Exhibition Market Bash]
+event_descriptions = [
+  "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view.",
+  "Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.",
+  "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop.",
+  "Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality.",
+  "Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI."
+]
 
 users = names.each do |name|
   User.create(name: "#{name}",
@@ -27,10 +34,11 @@ dates = []
 end
 
 (1..10).each do |i|
-  Event.create(title: "#{event_descriptions.sample} #{event_types.sample}",
+  Event.create(title: "#{event_names.sample} #{event_types.sample}",
                location: "#{locations.sample}",
-                date: "#{dates.sample}",
-                creator_id: "#{User.all.to_a.sample.id}")
+               date: "#{dates.sample}",
+               description: "#{event_descriptions.sample}",
+               creator_id: "#{User.all.to_a.sample.id}")
   end
 
 (1..30).each do |i|
