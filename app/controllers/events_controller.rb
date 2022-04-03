5130297b84bc0f12cf1c3d8c6696c306c10935e9
PlaustralCL <plaustralcl@gmail.com>
2022-04-03 10:31:34 -0400

    Add event description to the allowed params list

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :location, :date)
+    params.require(:event).permit(:title, :location, :description, :date)
   end
 end
