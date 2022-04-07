5c5e8214631ce7786569ff0067890eec854816a0
PlaustralCL <plaustralcl@gmail.com>
2022-04-07 07:37:56 -0400

    Validate user is signed in before creating a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @events = Event.all
     @future_events = Event.future
